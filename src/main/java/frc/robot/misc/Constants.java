12a7e747a2bacb7c3591dfe4419eb5a55e52aba4
Ryan <rybridgwood@gmail.com>
2022-04-01 20:41:02 -0400

    voltage values Constants fixed

--- src/main/java/frc/robot/misc/Constants.java
+++ src/main/java/frc/robot/misc/Constants.java
@@ -83,12 +83,12 @@ public final class Constants {
         public static final double kaVoltSecondsSquaredPerMeter = 0.44158;
     
         // Example value only - as above, this must be tuned for your drive!
-        public static final double kPDriveVel = 8.5;
+        public static final double kPDriveVel = 0.66725;
     }
 
     public static final class AutoConstants {
-        public static final double kMaxSpeedMetersPerSecond = 3;
-        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxSpeedMetersPerSecond = 2;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 2;
         
         // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
         public static final double kRamseteB = 2;
